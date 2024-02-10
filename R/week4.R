29b217fd548d0a2367d6ed88c13f37de2f8cb466
Toni Merhar <merha013@umn.edu>
2024-02-10 08:13:34 -0600

    Updated to step 10

--- R/week4.R
+++ R/week4.R
@@ -3,4 +3,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
 # Data Import
-import_tbl <- read_delim("../data/week4.dat")
+import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+str(import_tbl)
+wide_tbl <- separate_wider_delim(import_tbl, qs, delim=" - ", names = c("q1", "q2", "q3", "q4", "q5"))
+wide_tbl <- sapply(wide_tbl(c("q1", "q2","q3", "q4", "q5")), as.integer)
